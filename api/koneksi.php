bd6ea644e667dc193333e02986e5d8e0c9a9778f
SatrioXD <satriodwisetiawan@student.uns.ac.id>
2026-04-28 15:35:59 +0700

    memperbarui koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,10 +1,11 @@
 <?php
-$host     = "localhost";
-$user     = "root";       // Ganti dengan username MySQL Anda
-$password = "";           // Ganti dengan password MySQL Anda
+$host     = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
+$port = 4000;
+$user     = "DLDDRzUzmUhedCS.root";       // Ganti dengan username MySQL Anda
+$password = "5Bh4vn4GMldKXjsP";           // Ganti dengan password MySQL Anda
 $database = "cepat";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $port, $user, $password, $database);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
